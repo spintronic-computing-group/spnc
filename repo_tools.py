be563869e1d301c1ead5519846b8ed147d4e8dcb
Alexander Welbourne <aw562@cam.ac.uk>
2020-06-23 15:15:02 +0100

    Add multiple path search to repo_tools

--- repo_tools.py
+++ repo_tools.py
@@ -6,6 +6,8 @@ Functions
 repo_path_finder(searchpath, reponame)
     Searches for a repository and adds it and its parent directory to path
 
+repos_path_finder(searchpaths, reponames)
+    Find a list of repositories on list of search paths. Add them to sys.paths
 """
 
 import sys
@@ -56,3 +58,30 @@ def repo_path_finder(searchpath,reponame):
         sys.path.index(str(repopath))
     except ValueError:
         sys.path.append(str(repopath))
+
+def repos_path_finder(searchpaths, reponames):
+    """
+    Find a list of repositories on list of search paths. Add them to sys.paths
+
+    Utilises repo_path_finder for each entry
+    Adds both parent directory and repo directory
+    Will use the first match it finds on each search path
+
+    Parameters
+    ----------
+    searchpaths : tuple of path
+        paths to search
+    reponames : tuple of str
+        the names of the repository to be added
+
+    Examples
+    --------
+        repo_path_finder( (Path('path1'), Path('path2') ),
+                         ( 'machine_learning_library', ) )
+            This will search for the machine_learning_library repo under
+            'path1' & 'path2'
+    """
+
+    for searchpath in searchpaths:
+        for reponame in reponames:
+            repo_path_finder(searchpath, reponame)
