f46950c843cc726a0876613c3fea981006a348db
Alexander Welbourne <aw562@cam.ac.uk>
2020-06-26 15:55:58 +0100

    Add notebook for quick plotting
    - For ideas/quick presentation

---
+++ notebooks/SPNC_plotting/SPNC_plotting.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python (SPNC)
+#     language: python
+#     name: spnc
+# ---
+
+# %% [markdown]
+# # SPNC plotting
+
+# %% [markdown]
+# *This is a notebook is used for quick plotting of ideas*
+
+# %%
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import constants
+
+# Magic to choose matplotlib backend (used mostly)
+%matplotlib widget
+
+#3D plotting
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+# %%
