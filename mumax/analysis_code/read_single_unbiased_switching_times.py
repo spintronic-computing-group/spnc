74a34c1687e458aa4d5437410ec0253e43953bb1
Alexander Welbourne <aw562@cam.ac.uk>
2021-01-15 14:54:16 +0000

    Adjust figure sizes/aspect ratio/fonts/linewidths/etc

--- mumax/analysis_code/read_single_unbiased_switching_times.py
+++ mumax/analysis_code/read_single_unbiased_switching_times.py
@@ -12,6 +12,7 @@ import scipy.stats as ss
 # root.update()
 # print(folder_name)
 folder_name = 'input'
+output = 'output'
 
 #Read the data
 df = pd.read_csv(folder_name+'/table.txt',sep='\t')
@@ -58,17 +59,22 @@ error = P[1]/np.sqrt(total_switches)
 #display the results
 print('Tau = ' + str(round(P[1])) +' +/- ' + str(int(round(error))) + " ns")
 
-plt.figure(figsize=(7,6),dpi=200)
+fsz = 10
+figurewidth = 3.37 #inches (single column)
+figureaspect = 1
+figureheight = figurewidth*figureaspect
+plt.figure(figsize=[figurewidth,figureheight],dpi=1200)
 plt.hist(switching_times,edgecolor="blue",alpha=.5,bins=20,density=True)
-plt.plot(T,P_theo,color="red",lw=3,linestyle='--',label="Exponential Fit ("+r'$\tau = $' + str(round(P[1])) +r'$\pm$' + str(int(round(error))) + "ns)")
+plt.plot(T,P_theo,color="red",lw=1.5,linestyle='--',label="Exponential Fit ("+r'$\tau = $' + str(round(P[1])) +r'$\pm$' + str(int(round(error))) + "ns)")
 #plt.yscale("log")
-plt.legend(loc="best",fontsize=14)
-plt.ylabel("Density of switching times",fontsize=14)
-plt.xlabel("Time (in ns)",fontsize=14)
+plt.legend(loc="best",fontsize=fsz*0.9)
+plt.ylabel("Density of switching times",fontsize=fsz)
+plt.xlabel("Time (in ns)",fontsize=fsz)
 plt.xlim(0,600)
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
+plt.xticks(fontsize=fsz)
+plt.yticks(fontsize=fsz)
 #plt.ylim(1e-6,1e-1)
+plt.savefig('output/'+'switching_density_plot.pdf',format='pdf',transparent=True,dpi=1200,bbox_inches='tight')
 plt.show()
 
 # Sort switches and plot that way
@@ -79,19 +85,24 @@ def exp_func(t,tau):
     return np.exp(-t/tau)
 
 
-fsz = 20
-plt.figure(figsize=(7,6),dpi=200)
-plt.scatter(sorted_switches,p_not_switching,color='black')
+fsz = 10
+figurewidth = 3.37 #inches (single column)
+figureaspect = 1
+figureheight = figurewidth*figureaspect
+plt.figure(figsize=[figurewidth,figureheight],dpi=1200)
+plt.scatter(sorted_switches,p_not_switching,s=1.5,color='black')
 P_theo2 = ss.expon.pdf(sorted_switches, *P_new)/ss.expon.pdf(0, *P_new)
-plt.plot(sorted_switches,P_theo2,color="red",lw=3,linestyle='--',label="Exponential Fit ("+r'$\tau = $' + str(round(P[1])) +r'$\pm$' + str(int(round(error))) + "ns)")
+plt.plot(sorted_switches,P_theo2,color="red",lw=1.5,alpha=0.8,linestyle='--',
+         label="Exponential Fit ("+r'$\tau = $' + str(round(P[1])) +r'$\pm$' + str(int(round(error))) + "ns)")
 
-plt.legend(loc="best",fontsize=fsz)
+plt.legend(loc="best",fontsize=fsz*0.9)
 plt.ylabel("Probability of not switching",fontsize=fsz)
 plt.xlabel("Time (in ns)",fontsize=fsz)
 plt.xticks(fontsize=fsz)
 plt.yticks(fontsize=fsz)
 plt.xlim(0,600)
 plt.ylim(0,1)
+plt.savefig('output/'+'prob_not_switching.pdf',format='pdf',transparent=True,dpi=1200,bbox_inches='tight')
 plt.show()
 
 # Does mean of time period converge with increasing number of points?
@@ -115,11 +126,15 @@ for idx, i in np.ndenumerate(number_of_results):
     meanspdf[idx]=tau
     errorspdf[idx]=tauerror
 
-
-plt.figure(figsize=(7,6),dpi=200)
-plt.errorbar(number_of_results, meanspdf, yerr =errorspdf)
-plt.ylabel(r'PDF observation of $\tau$ / ns',fontsize=14)
-plt.xlabel("Number of switches observed",fontsize=14)
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
+fsz = 10
+figurewidth = 3.37 #inches (single column)
+figureaspect = 1
+figureheight = figurewidth*figureaspect
+plt.figure(figsize=[figurewidth,figureheight],dpi=1200)
+plt.errorbar(number_of_results, meanspdf, yerr =errorspdf,linewidth=1)
+plt.ylabel(r'PDF observation of $\tau$ / ns',fontsize=fsz)
+plt.xlabel("Number of switches observed",fontsize=fsz)
+plt.xticks(fontsize=fsz)
+plt.yticks(fontsize=fsz)
+plt.savefig('output/'+'estimates_of_tau.pdf',format='pdf',transparent=True,dpi=1200,bbox_inches='tight')
 plt.show()
