79053af417a83a00eaa1a7151c31a3b71db6a5d7
mt1awe <a.welbourne@sheffield.ac.uk>
2022-06-20 17:03:42 +0100

    Add notebook for experimenting with thermal variation

---
+++ notebooks/SPNC_thermals/SPNC_thermals.py
@@ -0,0 +1,14 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.7
+# ---
+
+# %% [markdown]
+# # SPNC thermals
+# Code for experimenting with the effects of thermal variation on ML performance
