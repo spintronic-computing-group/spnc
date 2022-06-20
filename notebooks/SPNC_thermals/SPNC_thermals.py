cd77944bdd186cc6377633c240e3eefbb165bf97
mt1awe <a.welbourne@sheffield.ac.uk>
2022-06-20 17:12:02 +0100

    Add initial code to thermals nb based on ml_response.py (APL plotting)

--- notebooks/SPNC_thermals/SPNC_thermals.py
+++ notebooks/SPNC_thermals/SPNC_thermals.py
@@ -7,8 +7,164 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3.7.6 ('SPNC')
+#     language: python
+#     name: python3
 # ---
 
 # %% [markdown]
 # # SPNC thermals
 # Code for experimenting with the effects of thermal variation on ML performance
+
+# %% [markdown]
+# Set up imports:
+
+# %%
+"""
+Import handeling and Dependancy info
+
+Local Dependancies
+------------------
+machine_learning_library  : v0.1.2
+    This repository will need to be on your path in order to work.
+    This is achieved with repo_tools module and a path find function
+    Add to the searchpath and repos tuples if required
+
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sort out relative paths
+import sys
+from pathlib import Path
+repodir = Path('..').resolve()
+try:
+    sys.path.index(str(repodir))
+except ValueError:
+    sys.path.append(str(repodir))
+
+#tuple of Path variables
+searchpaths = (Path.home() / 'repos', )
+#tuple of repos
+repos = ('machine_learning_library',)
+
+# local imports
+from SPNC import spnc
+#ML specific
+from SPNC.deterministic_mask import fixed_seed_mask, max_sequences_mask
+import SPNC.repo_tools
+SPNC.repo_tools.repos_path_finder(searchpaths, repos) #find ml library
+from single_node_res import single_node_reservoir
+import ridge_regression as RR
+from linear_layer import *
+from mask import binary_mask
+from utility import *
+from NARMA10 import NARMA10
+from sklearn.metrics import classification_report
+
+# %% [markdown]
+# Establish shared parameters
+
+# %%
+'''
+NARMA10 response
+'''
+
+# NARMA parameters
+Ntrain = 100
+Ntest = 50
+
+# Net Parameters
+Nvirt = 5
+m0 = 0.003
+bias = True
+
+# Misc parameters
+seed_NARMA = None
+fixed_mask = False
+spacer = 5
+
+# Resevoir parameters
+h = 0.4
+theta_H = 90
+k_s_0 = 0
+phi = 45
+beta_prime = 20
+
+theta = 0.4
+gamma = 0.132
+delay_feedback = 0
+params = {'theta': theta, 'gamma' : gamma,'delay_feedback' : delay_feedback,'Nvirt' : Nvirt}
+spnres = spnc.spnc_anisotropy(h,theta_H,k_s_0,phi,beta_prime)
+transform = spnres.gen_signal_fast_delayed_feedback
+
+spnreshigher = spnc.spnc_anisotropy(h,theta_H,k_s_0,phi,beta_prime)
+transformhigher = spnreshigher.gen_signal_fast_delayed_feedback
+
+# %% [markdown]
+# Data and net setup
+
+# %%
+# Lets get into it
+print("seed NARMA: "+str(seed_NARMA))
+u, d = NARMA10(Ntrain + Ntest,seed=seed_NARMA)
+
+x_train = u[:Ntrain]
+y_train = d[:Ntrain]
+x_test = u[Ntrain:]
+y_test = d[Ntrain:]
+
+print("Samples for training: ", len(x_train))
+print("Samples for test: ", len(x_test))
+
+# Net setup
+Nin = x_train[0].shape[-1]
+Nout = len(np.unique(y_train))
+
+print( 'Nin =', Nin, ', Nout = ', Nout, ', Nvirt = ', Nvirt)
+
+snr = single_node_reservoir(Nin, Nout, Nvirt, m0, res = transform)
+net = linear(Nin, Nout, bias = bias)
+
+# %% [markdown]
+# Training and testing:
+
+# %%
+# Training
+S_train, J_train = snr.transform(x_train,params)
+np.size(S_train)
+seed_training = 1234
+RR.Kfold_train(net,S_train,y_train,10, quiet = False)
+
+# Testing
+S_test, J_test = snr.transform(x_test,params)
+#test with the other transform...
+snr.res = transformhigher
+S_test_higher, J_test_higher = snr.transform(x_test,params)
+
+
+print("Spacer NRMSE:"+str(spacer))
+pred = net.forward(S_test)
+pred_higher = net.forward(S_test_higher)
+plt.plot(pred)
+plt.plot(pred_higher)
+plt.show()
+np.size(pred)
+error = MSE(pred[spacer:], y_test[spacer:])
+predNRMSE = NRMSE(pred[spacer:], y_test[spacer:])
+print('Error and NRMSE for normal testing', error, predNRMSE)
+
+error_higher = MSE(pred_higher[spacer:], y_test[spacer:])
+predNRMSE_higher = NRMSE(pred_higher[spacer:], y_test[spacer:])
+print('Error and NRMSE for higher temp testing', error_higher, predNRMSE_higher)
+
+plt.plot( np.linspace(0.0,1.0), np.linspace(0.0,1.0),'k--')
+plt.plot(y_test[spacer:],pred[spacer:],'o')
+plt.show()
+
+plt.plot( np.linspace(0.0,1.0), np.linspace(0.0,1.0),'k--')
+plt.plot(y_test[spacer:],pred_higher[spacer:],'o')
+plt.show()
