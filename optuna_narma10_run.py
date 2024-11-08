2599dd43740d88714462f4d3bf67fbcf4fc72cc9
Chen_S <zchen153@sheffield.ac.uk>
2024-11-08 22:02:46 +0000

    Update optuna_narma10_run.py

--- optuna_narma10_run.py
+++ optuna_narma10_run.py
@@ -1,36 +1,37 @@
 
 from optuna_narma10_study import create_study
-from optuna_narma10_spnc_heterogenous import objective
+from optuna_narma10_objective import objective
 
 
 
 if __name__ == "__main__":
 
+    hyperparameter_ranges = {
+    'Nvirt': (50, 100),
+    'gamma': (0.11, 0.12),    
+    'h': (0.35, 0.45),
+    'm0': (0.003, 0.0035),
+    'theta': (0.28, 0.35),
+    'num_instances': (3, 7),        
+    'deltabeta_range': (-5.0, 5.0), 
+    'weight_range': (0.0, 1.0)      
+    }
 
-    params = {'theta': 0.3,'gamma' : .113,'delay_feedback' : 0,'Nvirt' : 400}
+
 
     bias = True
 
-    hyperparameter_ranges = {
-        'Nvirt': (400, 450),
-        'gamma': (0.111, 0.112),    
-        'h': (0.39, 0.41),
-        'm0': (0.003, 0.0031),
-        'theta': (0.3, 0.301),
-        'num_instances': (1, 2),        
-        'deltabeta_range': (-5.0, 5.0), 
-        'weight_range': (0.0, 1.0)      
-    }
+
 
     Ntrain = 2000
     Ntest = 1000
 
     temp_params = {
-        'beta_prime': 35,    # 初始 beta_prime
-        'beta_ref': 35,     # 常量 beta_cons
-        'step': 0.875,       # beta_prime 步长
-        'beta_left': 33.25,  # 左侧 beta_prime 范围
-        'beta_right': 36.75  # 右侧 beta_prime 范围
+        'beta_prime': 20,    # 初始 beta_prime
+        'beta_ref': 20,     # 常量 beta_cons
+        'step': 1,       # beta_prime 步长
+        'beta_left': 18.9,  # 左侧 beta_prime 范围
+        'beta_right': 21.1  # 右侧 beta_prime 范围
     }
 
     # Create a list to collect all the trials
@@ -38,7 +39,7 @@ if __name__ == "__main__":
     study = create_study()
 
     study.optimize(
-        lambda trial:objective(trial, hyperparameter_ranges,  temp_params,True, params),
-        n_trials=2
+        lambda trial:objective(trial, Ntrain, Ntest, hyperparameter_ranges,  temp_params,True),
+        n_trials=100
     )
 
