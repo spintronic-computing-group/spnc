03641a18d85c33bace32dad48e1f74fb659eb340
mt1awe <a.welbourne@sheffield.ac.uk>
2024-11-14 11:46:34 +0000

    Do some testing with values more similar to Chen's

--- narma10_SPNC_anisotropy.py
+++ narma10_SPNC_anisotropy.py
@@ -10,7 +10,7 @@ Ntest = 1000
 
 # Net Parameters
 Nvirt = 400
-m0 = 2.7e-2
+m0 = 1e-2
 bias = True
 
 # Resevoir parameters
@@ -18,16 +18,16 @@ h = 0.4
 theta_H = 90
 k_s_0 = 0
 phi = 45
-beta_prime = 20
-params = {'theta': 10,'gamma' : .132,'delay_feedback' : 1,'Nvirt' : Nvirt}
+beta_prime = 10
+params = {'theta': 1/3,'gamma' : .25,'delay_feedback' : 0,'Nvirt' : Nvirt}
 spn = spnc_anisotropy(h,theta_H,k_s_0,phi,beta_prime)
 transform = spn.gen_signal_fast_delayed_feedback
 
 # DO IT
-(y_test,y_pred)=ml.spnc_narma10(Ntrain, Ntest, Nvirt, m0, bias, transform, params, seed_NARMA=1234,fixed_mask=True, return_outputs=True)
+(y_test_f,y_pred_f)=ml.spnc_narma10(Ntrain, Ntest, Nvirt, m0, bias, transform, params, seed_NARMA=1234,fixed_mask=True, return_outputs=True)
 
 y_test_df = pd.DataFrame(y_test)
 y_pred_df = pd.DataFrame(y_pred)
 
-y_test_df.to_csv('../csv_files/y_test.csv')
-y_pred_df.to_csv('../csv_files/y_pred.csv')
+# y_test_df.to_csv('../csv_files/y_test.csv')
+# y_pred_df.to_csv('../csv_files/y_pred.csv')
