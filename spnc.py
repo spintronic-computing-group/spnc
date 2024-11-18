2ceae18575ede614983436e2d798f101c433737e
Chen_S <zchen153@sheffield.ac.uk>
2024-11-18 14:40:46 +0000

    temporary comment the interpolation plot

--- spnc.py
+++ spnc.py
@@ -462,46 +462,46 @@ class spnc_anisotropy:
 
             print('p1 in train & fast:', self.p1)
 
-            print('==================== parameters after changing p1 ====================')
+            # print('==================== parameters after changing p1 ====================')
 
-            calculate_energy_barriers(self)
+            # calculate_energy_barriers(self)
             
-            if self.compute_interpolation:
-                (self.f_theta_1,self.f_theta_2,self.f_e_12_small,self.f_e_21_small,self.f_e_12_big,self.f_e_21_big) = functions_energy_barriers(self,self.k_s_lim)
-                (self.f_p1_eq,self.f_om_tot) = self.calculate_f_p1_om(self.k_s_lim)
+            # if self.compute_interpolation:
+            #     (self.f_theta_1,self.f_theta_2,self.f_e_12_small,self.f_e_21_small,self.f_e_12_big,self.f_e_21_big) = functions_energy_barriers(self,self.k_s_lim)
+            #     (self.f_p1_eq,self.f_om_tot) = self.calculate_f_p1_om(self.k_s_lim)
 
 
 
-            import scipy.interpolate
-            import matplotlib.pyplot as plt
+            # import scipy.interpolate
+            # import matplotlib.pyplot as plt
 
-            interpolations = []
+            # interpolations = []
 
-            for attr, value in vars(self).items():
-                if isinstance(value, scipy.interpolate.interpolate.interp1d):
-                    # collect interpolation functions
-                    x_data = value.x
-                    y_data = value.y
-                    interpolations.append((attr, x_data, y_data))
-                elif isinstance(value, np.ndarray):
-                    print(f"{attr}: shape: {value.shape}")
-                    print(f"  value: {value}")
-                elif callable(value):
-                    print(f"{attr}: callable")
-                else:
-                    print(f"{attr}: {value}")
+            # for attr, value in vars(self).items():
+            #     if isinstance(value, scipy.interpolate.interpolate.interp1d):
+            #         # collect interpolation functions
+            #         x_data = value.x
+            #         y_data = value.y
+            #         interpolations.append((attr, x_data, y_data))
+            #     elif isinstance(value, np.ndarray):
+            #         print(f"{attr}: shape: {value.shape}")
+            #         print(f"  value: {value}")
+            #     elif callable(value):
+            #         print(f"{attr}: callable")
+            #     else:
+            #         print(f"{attr}: {value}")
 
-            # plot interpolation functions
-            for attr, x_data, y_data in interpolations:
-                plt.figure()
-                plt.plot(x_data, y_data, label=f"{attr}")
-                plt.title(f"{attr} interpolation")
-                plt.xlabel("x")
-                plt.ylabel("y")
-                plt.legend()
-                plt.show()
+            # # plot interpolation functions
+            # for attr, x_data, y_data in interpolations:
+            #     plt.figure()
+            #     plt.plot(x_data, y_data, label=f"{attr}")
+            #     plt.title(f"{attr} interpolation")
+            #     plt.xlabel("x")
+            #     plt.ylabel("y")
+            #     plt.legend()
+            #     plt.show()
 
-            print('==================== parameters after changing p1 ====================')
+            # print('==================== parameters after changing p1 ====================')
 
             theta_T = params['theta']
             self.k_s = 0
