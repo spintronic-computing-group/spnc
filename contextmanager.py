10be8b1570978bbe5ef522df96574411db63c9eb
Chen_S <zchen153@sheffield.ac.uk>
2024-11-20 12:54:20 +0000

    add a no_print function

---
+++ contextmanager.py
@@ -0,0 +1,13 @@
+import sys
+from contextlib import contextmanager
+import os
+
+@contextmanager
+def no_print():
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
