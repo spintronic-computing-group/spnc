5ef877026a00d39758d9a2b11b66a9208c44b6b9
zf_Chen <109740983+VvvpC@users.noreply.github.com>
2024-11-26 20:56:31 +0000

    Update narma10_spnc_heterogenous.py

--- narma10_spnc_heterogenous.py
+++ narma10_spnc_heterogenous.py
@@ -6,8 +6,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 from single_node_heterogenous_reservoir import single_node_heterogenous_reservoir
 
 # NARMA parameters
@@ -39,4 +37,4 @@ weights = [1,0,0]  # weights 长度应与 deltabeta_list 匹配
 
 # DO IT
 
-beta_primes_temp, nrmse_temp =ml.spnc_narma10_heterogenous(Ntrain,Ntest,Nvirt,gamma, beta_prime, beta_ref, deltabeta_list,h,theta,m0,step,beta_left,beta_right,*weights, bias = bias,params = params,seed_NARMA=1234)
+beta_primes_temp, nrmse_temp =ml.spnc_narma10_heterogenous(Ntrain, Ntest, Nvirt, gamma, beta_prime, beta_ref, deltabeta_list,h,theta,m0,step,beta_left,beta_right,*weights, bias = bias,params = params,seed_NARMA=1234)
